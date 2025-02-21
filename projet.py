c385254f279ec023de794e6a8a76a5924590dc2e
Thibault Ducruet <thibault.ducruet@etu.univ-lyon1.fr>
2025-02-21 11:26:39 +0100

    marche pas encore

--- projet.py
+++ projet.py
@@ -1,7 +1,8 @@
 tabId = []
 tabProfit = []
 tabPoids = []
-maxCap = 0
+nbItem = ""
+maxCap = ""
 
 
 #Remplissage des tableaux qui représente le graphe
@@ -23,5 +24,69 @@ def lireFichier(f):
     fichier.close()
 
 lireFichier("data\pi-12-100-1000-001.kna")
-print(tabProfit)
-print(tabPoids)
+
+def tabuSearch():
+    maxIter = 12
+    xmin = solutionInitial()
+    x = xmin
+    fmin = sommeProfit(xmin)
+    i = 0
+    tabTabu = []
+    for i in range (0,maxIter):
+        listVoisin = listerVoisin(tabTabu, x)
+        xbis = maximiserProfit(listVoisin) 
+        deltaf = sommeProfit(xbis) - sommeProfit(x)
+        if deltaf >= 0 :
+            if(len(tabTabu)==1):
+                tabTabu.clear()
+            tabTabu.append(xbis)
+        if sommeProfit(xbis) < sommeProfit(xmin):
+            xmin = xbis
+            fmin = sommeProfit(xbis)
+        x = xbis
+    return xmin, fmin
+
+def sommeProfit(listItem):
+    sum = 0
+    i=0
+    for i in range(0, nbItem):
+        if listItem[i] == 1 :
+            sum += int(tabProfit[i])
+    return sum
+
+def isAccepted(listItem):
+    sum = 0
+    i=0
+    for i in range(0, nbItem):
+        if listItem[i] == 1 :
+            sum += int(tabPoids[i])
+    return sum < maxCap
+
+def listerVoisin(tabTabu, listItem):
+    listVoisin = []
+    i=0
+    for i in range(0,nbItem):
+        voisin = listItem.copy()
+        if voisin[i] == 0 :
+            voisin[i] = 1
+        else :
+            voisin[i] = 0
+        if not voisin in tabTabu and isAccepted(voisin):
+            listVoisin.append(voisin)
+    return listVoisin
+
+def maximiserProfit(listVoisin):
+    max=0
+    betterVoisin = []
+    i=0
+    for i in range(0, len(listVoisin)):
+        if max < sommeProfit(listerVoisin[i]) : 
+            max = sommeProfit(listerVoisin[i])
+            betterVoisin = listerVoisin[i]
+    return betterVoisin
+
+def solutionInitial():
+    return [0] * nbItem
+
+print(maxCap)
+print(tabuSearch())
